e4901cb0f1cd1d08f6dfd15127da8d71676a39e5
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-03-26 16:18:13 -0300

    Delete src/main/java/com/tuyo/associations/onetoone directory

--- src/main/java/com/tuyo/associations/onetoone/repos/LicencaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tuyo.associations.onetoone.repos;
-
-import com.tuyo.associations.onetoone.entities.Licenca;
-import org.springframework.data.repository.CrudRepository;
-
-public interface LicencaRepository extends CrudRepository<Licenca, Long> {
-
-}
