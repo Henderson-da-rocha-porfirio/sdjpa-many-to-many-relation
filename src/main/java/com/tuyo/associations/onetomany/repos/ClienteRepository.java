c5254918e5f2175203f1def27da4b9a32c3c5d77
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-03-26 16:18:05 -0300

    Delete src/main/java/com/tuyo/associations/onetomany directory

--- src/main/java/com/tuyo/associations/onetomany/repos/ClienteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tuyo.associations.onetomany.repos;
-
-import com.tuyo.associations.onetomany.entities.Cliente;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ClienteRepository extends CrudRepository<Cliente, Long> {
-
-}
